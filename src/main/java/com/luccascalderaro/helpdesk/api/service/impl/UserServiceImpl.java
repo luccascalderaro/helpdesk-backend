6dc4fa0eec0286a4b69f43a47ad7519437580756
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-25 19:25:41 -0300

    Exception NotFound e Service User criados

---
+++ src/main/java/com/luccascalderaro/helpdesk/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.luccascalderaro.helpdesk.api.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.luccascalderaro.helpdesk.api.entity.User;
+import com.luccascalderaro.helpdesk.api.exception.ObjectNotFoundException;
+import com.luccascalderaro.helpdesk.api.repository.UserRepository;
+import com.luccascalderaro.helpdesk.api.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User findByEmail(String email) {
+		
+		return this.userRepository.findByEmail(email);
+	}
+
+	@Override
+	public User createOrUpdate(User user) {
+		
+		return this.userRepository.save(user);
+	}
+
+	@Override
+	public User findById(String id) {
+		Optional<User> user = this.userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado") );
+	}
+
+	@Override
+	public void delete(String id) {
+		
+		this.userRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public Page<User> findAll(int page, int count) {
+		PageRequest pages = PageRequest.of(page, count);
+		return this.userRepository.findAll(pages);
+	}
+
+}
