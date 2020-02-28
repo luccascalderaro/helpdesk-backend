9ef56fb62f8eba557d750c04df134338d878ac50
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-27 21:24:07 -0300

    Metodos findById, delete e FindAll criados

--- src/main/java/com/luccascalderaro/helpdesk/api/resource/UserResource.java
+++ src/main/java/com/luccascalderaro/helpdesk/api/resource/UserResource.java
@@ -11,7 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -106,5 +108,42 @@ public class UserResource {
 		}
 	}
 	
+	@GetMapping(value="/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> findById(@PathVariable ("id") String id){
+		Response<User> response = new Response<User>();
+		User user = userService.findById(id);
+		if(user == null) {
+			response.getErrors().add("Registro não encontrado pelo id: " + id);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(user);
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") String id){
+		Response<String> response = new Response<String>();
+		User user = userService.findById(id);
+		if(user == null) {
+			response.getErrors().add("Registro não encontrado pelo id: " + id);
+			return ResponseEntity.badRequest().body(response);
+		}
+		userService.delete(id);
+		return ResponseEntity.ok(new Response<String>());
+			
+	}
+	
+	@GetMapping(value = "{page}/{count}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<Page<User>>> finAll(@PathVariable int page, @PathVariable int count){
+		Response<Page<User>> response = new Response<Page<User>>();
+		Page<User> users = userService.findAll(page, count);
+		response.setData(users);
+		return ResponseEntity.ok(response);
+	}
 
 }
