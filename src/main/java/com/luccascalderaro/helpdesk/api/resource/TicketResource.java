ff3e7e41f9b57c84d03c682b49cd27aa26224f1e
Luccas Calderaro <luccashmc@hotmail.com>
2020-02-29 13:45:39 -0300

    Delete do ticket Criado

--- src/main/java/com/luccascalderaro/helpdesk/api/resource/TicketResource.java
+++ src/main/java/com/luccascalderaro/helpdesk/api/resource/TicketResource.java
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -100,35 +101,34 @@ public class TicketResource {
 	@PutMapping()
 	@PreAuthorize("hasAnyRole('CUSTOMER')")
 	public ResponseEntity<Response<Ticket>> update(HttpServletRequest request, @RequestBody Ticket ticket,
-			BindingResult result){
+			BindingResult result) {
 		Response<Ticket> response = new Response<Ticket>();
-		
+
 		try {
 			validateUpdateTicket(ticket, result);
 			if (result.hasErrors()) {
 				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
 				return ResponseEntity.badRequest().body(response);
 			}
-			
+
 			Ticket ticketCurrent = ticketService.findById(ticket.getId());
 			ticket.setStatus(ticketCurrent.getStatus());
 			ticket.setUser(ticketCurrent.getUser());
 			ticket.setDate(ticketCurrent.getDate());
 			ticket.setNumber(ticketCurrent.getNumber());
-			if(ticketCurrent.getAssignedUser() != null) {
+			if (ticketCurrent.getAssignedUser() != null) {
 				ticket.setAssignedUser(ticketCurrent.getAssignedUser());
 			}
 			Ticket ticketPersisted = (Ticket) ticketService.createOrUpdate(ticket);
 			response.setData(ticketPersisted);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			response.getErrors().add(e.getMessage());
 			return ResponseEntity.badRequest().body(response);
-			
+
 		}
 		return ResponseEntity.ok(response);
 	}
-	
-	
+
 	private void validateUpdateTicket(Ticket ticket, BindingResult result) {
 		if (ticket.getId() == null) {
 			result.addError(new ObjectError("Ticket", "Id não informado"));
@@ -141,29 +141,43 @@ public class TicketResource {
 		}
 
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	@PreAuthorize("hasAnyRole('CUSTOMER', 'TECHNICIAN')")
-	public ResponseEntity<Response<Ticket>> findById(@PathVariable("id") String id){
+	public ResponseEntity<Response<Ticket>> findById(@PathVariable("id") String id) {
 		Response<Ticket> response = new Response<Ticket>();
 		Ticket ticket = ticketService.findById(id);
-		if(ticket == null) {
-			response.getErrors().add("Registro não encontrado pelo id: " + id )	;
+		if (ticket == null) {
+			response.getErrors().add("Registro não encontrado pelo id: " + id);
 			return ResponseEntity.badRequest().body(response);
-			}
-		
+		}
+
 		List<ChangeStatus> changes = new ArrayList<ChangeStatus>();
 		Iterable<ChangeStatus> changesCurrent = ticketService.listChangeStatus(ticket.getId());
 		for (Iterator<ChangeStatus> iterator = changesCurrent.iterator(); iterator.hasNext();) {
 			ChangeStatus changeStatus = (ChangeStatus) iterator.next();
 			changeStatus.setTicket(null);
 			changes.add(changeStatus);
-			
+
 		}
 		ticket.setChanges(changes);
 		response.setData(ticket);
 		return ResponseEntity.ok(response);
-			
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@PreAuthorize("hasAnyRole('CUSTOMER')")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") String id) {
+		Response<String> response = new Response<String>();
+		Ticket ticket = ticketService.findById(id);
+		if (ticket == null) {
+			response.getErrors().add("Registro não encontrado pelo id: " + id);
+			return ResponseEntity.badRequest().body(response);
 		}
 		
+		ticketService.delete(id);
+		return ResponseEntity.ok(new Response<String>());
+	}
+
 }
